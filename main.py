e773740f1a4a3e8505484ff92a8a971d464354b4
wesleygas <wesleygl16@gmail.com>
2019-03-19 17:31:49 -0300

    add name

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import utils
 
 class Barra():
 
-    def __init__(self, p1, p2, E_modulus, cs_area):
+    def __init__(self, name, p1, p2, E_modulus, cs_area):
+        self.name = name
         self.p1 = p1
         self.p2 = p2
         self.length = utils.barLength(p1,p2)
